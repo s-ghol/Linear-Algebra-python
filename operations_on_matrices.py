6b04736d0925ed70e0c8e6156673de8aaa1697de
Salim Gholami 👻 <salim.gholami@outlook.com>
2021-03-13 00:00:56 +1300

    - matrix multiplication

--- operations_on_matrices.py
+++ operations_on_matrices.py
@@ -49,24 +49,61 @@ import copy
 #         print(result)
 #         return result
 
-
+# Scalar multiplication
 # def m_multiply_by_c(A, scalar):
 #     result = []
 #     for i in A:
 #         result.append(list(map(lambda x: scalar * x, i)))
 
+# Transpose A
+# def m_transpose(A):
+#     C = copy.deepcopy(A)
+#     row_A = len(A)
+#     col_A = len(A[0])
+#     # change a(ij) to a(ji) to perform the transpose
+#     for i in range(row_A):
+#         for j in range(col_A):
+#             C[j][i] = A[i][j]
+#     print(C)
+#     return C
 
-def m_transpose(A):
-    C = copy.deepcopy(A)
+
+# A = [[1, 2], [3, 4]]  # solution: [[1,3],[2,4]]
+# m_transpose(A)
+
+
+# Matrix Multiplication of A and B
+def m_multiplication(A, B):
+    """
+    Requirement: col_A must equal row_B, which will yield a matrix c of size row_A X col_B
+    :param A: Matrix elements of A
+    :type A: list
+    :param B: Matrix elements of B
+    :type B: list
+    :return C: multiplication result in a list C
+    :rtype: list
+    """
     row_A = len(A)
+    row_B = len(B)
     col_A = len(A[0])
-    # change a(ij) to a(ji) to perform the transpose
-    for i in range(row_A):
-        for j in range(col_A):
-            C[j][i] = A[i][j]
+    col_B = len(B[0])
+    if col_A == row_B:
+        C = [[0]*col_A]*row_B
+        for i in range(row_A):  # loop through the rows of A
+            for j in range(col_B):  # loop through cols of B
+                s = 0
+                for k in range(row_B):  # loop through rows of B
+                    s += A[i][k] * B[k][j]
+                C[i][j] = s
+    else:
+        print("False; size mismatch")
     print(C)
     return C
 
-#
-# A = [[1, 2], [3, 4]]  # solution: [[1,3],[2,4]]
-# m_transpose(A)
+
+
+A = [[1, 1],
+     [1, 1]]
+B = [[1, 1],
+     [1, 1]]
+m_multiplication(A, B)
