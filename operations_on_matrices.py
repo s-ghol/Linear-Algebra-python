bdb54b8f095ec53c9aaa1bf1268a29d3eaa5476e
Salim Gholami 👻 <salim.gholami@outlook.com>
2021-03-19 23:13:29 +1300

    - 2 by 2 Determinants function

--- operations_on_matrices.py
+++ operations_on_matrices.py
@@ -78,39 +78,56 @@ import copy
 # m_transpose(A)
 
 # -----------------------------------------------------------------------
+#
+# # Matrix Multiplication of A and B
+# def m_multiplication(A, B):
+#     """
+#     Requirement: col_A must equal row_B, which will yield a matrix c of size row_A X col_B
+#     :param A: Matrix elements of A
+#     :type A: list
+#     :param B: Matrix elements of B
+#     :type B: list
+#     :return C: multiplication result in a list C
+#     :rtype: list
+#     """
+#     row_A = len(A)
+#     row_B = len(B)
+#     col_A = len(A[0])
+#     col_B = len(B[0])
+#     if col_A != row_B:
+#         print("Inner matrix size do not match")
+#         sys.exit(0)
+#     C = [[None]*col_B for _ in range(row_A)] # (nxm)X(pxq) = (nxq) iff m = p
+#     for i in range(row_A):
+#         for j in range(col_B):
+#             s = 0
+#             for k in range(row_B):
+#                 s += A[i][k] * B[k][j]
+#             C[i][j] = s
+#     print(C)
+#     return C
+#
+# #test
+# E = [[2, 1],
+#      [1, 1],]
+# F = [[0, 1],
+#      [1, 2],
+#      [1, 2]]
+# m_multiplication(E, F)
+# -----------------------------------------------------------------------
 
-# Matrix Multiplication of A and B
-def m_multiplication(A, B):
-    """
-    Requirement: col_A must equal row_B, which will yield a matrix c of size row_A X col_B
-    :param A: Matrix elements of A
-    :type A: list
-    :param B: Matrix elements of B
-    :type B: list
-    :return C: multiplication result in a list C
-    :rtype: list
-    """
-    row_A = len(A)
-    row_B = len(B)
-    col_A = len(A[0])
-    col_B = len(B[0])
-    if col_A != row_B:
-        print("Inner matrix size do not match")
-        sys.exit(0)
-    C = [[None]*col_B for _ in range(row_A)] # (nxm)X(pxq) = (nxq) iff m = p
-    for i in range(row_A):
-        for j in range(col_B):
-            s = 0
-            for k in range(row_B):
-                s += A[i][k] * B[k][j]
-            C[i][j] = s
-    print(C)
-    return C
+def m_determinant(A):
+    m_size = len(A)
+    if m_size == 1:
+        print(A[0],"is not a square matrix")
+        return A[0]
+    elif m_size == 2:
+        determinant = A[0][0]* A[1][1] - A[0][1]* A[1][0]
+        print("det(A) = ", determinant)
+        return determinant
+    else:
+        pass
 
-#test
 E = [[2, 1],
-     [1, 1],]
-F = [[0, 1],
-     [1, 2],
-     [1, 2]]
-m_multiplication(E, F)
+     [2,3]]
+m_determinant(E)
