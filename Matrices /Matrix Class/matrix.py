e7a0795974aadaeb476bb89fa89a7b424d678ae4
Salim Gholami 👻 <salim.gholami@outlook.com>
2022-01-25 02:26:27 +1300

    matrix- rotation added. However __mul__ needs implementation so that we can perform linear transformation
    Signed-off-by: Salim Gholami 👻 <salim.gholami@outlook.com>

--- Matrices /Matrix Class/matrix.py
+++ Matrices /Matrix Class/matrix.py
@@ -19,12 +19,17 @@ class Matrix(object):
     ORTHOGONAL_PROJECTION = [[1, 0, 0],
                              [0, 1, 0],
                              [0, 0, 0]]
-    # Rotation matrix
-    D2_MATRIX_ROTATION = [[math.cos(ANGLE_IN_RADIAN), -math.sin(ANGLE_IN_RADIAN)],
-                          [math.sin(ANGLE_IN_RADIAN), math.cos(ANGLE_IN_RADIAN)]]
-    D3_MATRIX_ROTATION = [[1, 0, 0],
-                          [0, math.cos(ANGLE_IN_RADIAN), -math.sin(ANGLE_IN_RADIAN)],
-                          [0, math.sin(ANGLE_IN_RADIAN), math.cos(ANGLE_IN_RADIAN)]]
+    # Rotation matrix (TRANSFORM_MATRIX)
+
+    D3_MATRIX_ROTATION_x = [[1, 0, 0],
+                            [0, math.cos(ANGLE_IN_RADIAN), -math.sin(ANGLE_IN_RADIAN)],
+                            [0, math.sin(ANGLE_IN_RADIAN), math.cos(ANGLE_IN_RADIAN)]]
+    D3_MATRIX_ROTATION_y = [[math.cos(ANGLE_IN_RADIAN), 0, math.sin(ANGLE_IN_RADIAN)],
+                            [0, 1, 0],
+                            [-math.sin(ANGLE_IN_RADIAN), 0, math.cos(ANGLE_IN_RADIAN)]]
+    D3_MATRIX_ROTATION_z = [[math.cos(ANGLE_IN_RADIAN), -math.sin(ANGLE_IN_RADIAN), 0],
+                            [math.sin(ANGLE_IN_RADIAN), math.cos(ANGLE_IN_RADIAN), 0],
+                            [0, 0, 1]]
 
     def __init__(self, *args):
         """
@@ -32,13 +37,14 @@ class Matrix(object):
         :param args: list of lists
         :type args: list
         """
-        if len(args) == 1 and len(args[0]) == 0:  # if empty matrix
-            self.matrix = [0]
+        # todo: test the constructor
+        if len(args) == 1:  # if only one list is passed
+            self.matrix = args
             self.rows = 1
             self.cols = 1
 
         elif len(args) == 1 and len(args[0]) > 0:  # if matrix with one row
-            self.matrix = args[0]
+            self.matrix = args
             self.rows = len(args)
             self.cols = len(args[0])
 
@@ -50,6 +56,51 @@ class Matrix(object):
 
         else:
             raise ValueError("Matrix must be a list of lists")
+
+    def determinant(self) -> float:
+        """
+        Determinant of the matrix
+        :return: determinant
+        :rtype: int
+        """
+        if self.rows == self.cols:  # check if matrix is square
+            if self.rows == 1:  # if matrix is 1x1
+                return self.matrix[0][0]
+            elif self.rows == 2:  # if matrix is 2x2
+                return self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0]
+            elif self.rows == 3:  # if matrix is 3x3
+                #  {det}(A)=a*e*i- a*f*h - b*d*i + b*f*g + c*d*h - c*e*g (check README for details)
+                diagonal = self.matrix[0][0] * self.matrix[1][1] * self.matrix[2][2]
+                afh = self.matrix[0][0] * self.matrix[1][2] * self.matrix[2][1]
+                bdi = self.matrix[0][1] * self.matrix[1][0] * self.matrix[2][2]
+                bfg = self.matrix[0][1] * self.matrix[1][2] * self.matrix[2][0]
+                cdh = self.matrix[0][2] * self.matrix[1][0] * self.matrix[2][1]
+                ceg = self.matrix[0][2] * self.matrix[1][1] * self.matrix[2][0]
+                res = diagonal - afh - bdi + bfg + cdh - ceg
+                return res
+            elif self.rows == 4:
+                pass
+
+    # Linear Transformation; Rotation
+    def rotate_2d_x(self, angle: float) -> Vector:
+        """
+        Rotate 2d matrix around x axis
+        :param angle: angle in degree
+        :type angle: float
+        :return: rotated matrix
+        :rtype: Matrix
+        """
+        # requires a vector
+        angle = angle * self.ANGLE_IN_RADIAN  # convert to radian
+        D2_MATRIX_ROTATION = [[math.cos(angle), -math.sin(angle)],
+                              [math.sin(angle), math.cos(angle)]]
+        if self.rows >= 1 and self.cols == 1:  # if vector size
+            return Matrix(self.matrix).__mul__(D2_MATRIX_ROTATION)
+        else:
+            raise TypeError("Matrix must be a vector")
+
+    def lu_decomposition(self):
+        pass
 
     def rref(self, other):
         """
@@ -91,12 +142,7 @@ class Matrix(object):
     def __getitem__(self, key):
         return self.matrix[key]
 
+    def __mul__(self, other):
+        pass
 
-# #
-# a = Matrix([1, 2, 7], [4, 5, 1])
-# # print(a)
-# b = Matrix([1, 2, 6], [4, 5, 7])
-# # print(b, bb)
-# c = a.__add__(b)
-# print(c)
-# # print(a.matrix)
+
--- Matrices /Matrix Class/test_matrix.py
+++ Matrices /Matrix Class/test_matrix.py
@@ -12,3 +12,10 @@ class MyTestCase(unittest.TestCase):
     def test_add(self):
         """ Test add function """
         self.assertEqual(Matrix([[1, 2], [3, 4]]).__add__(Matrix([[1, 2], [3, 4]])), Matrix([[2, 4], [6, 8]]))
+
+    def test_determinant(self):
+        """ Test determinant function """
+        self.assertEqual(Matrix([2]).determinant(), 2)
+        self.assertEqual(Matrix([1, 2], [3, 4]).determinant(), -2)
+        self.assertEqual(Matrix([1, 2, 3], [4, 5, 6], [7, 8, 9]).determinant(), 0)
+        self.assertEqual(Matrix([3, 0, 1], [1, 2, 5], [-1, 4, 2]).determinant(), -42)
