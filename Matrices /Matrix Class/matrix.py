1ace2266e558dde6b569745e96221a0089d128a9
Salim Gholami 👻 <salim.gholami@outlook.com>
2022-02-06 01:45:27 +1300

    add more functions and tests
    Signed-off-by: Salim Gholami 👻 <salim.gholami@outlook.com>

--- Matrices /Matrix Class/matrix.py
+++ Matrices /Matrix Class/matrix.py
@@ -3,14 +3,15 @@
 # matrix.p_y
 # -----------------------------------------------------------------------
 import math
+from typing import Tuple
 
 
 class Matrix(object):
     """Matrix class"""
 
     # Class variables
-    PI = math.pi
-    ANGLE_IN_RADIAN = PI / 180
+    PI: float = math.pi
+    ANGLE_IN_RADIAN: float = PI / 180
     # Reflection matrix
     REFLECTION_MATRIX_x = [[1, 0], [0, -1]]
     REFLECTION_MATRIX_y = [[-1, 0], [0, 1]]
@@ -57,50 +58,13 @@ class Matrix(object):
         else:
             raise ValueError("Matrix must be a list of lists")
 
-    def determinant(self) -> float:
+    def dimension(self):
         """
-        Determinant of the matrix
-        :return: determinant
+        Dimension of the matrix
+        :return: dimension
         :rtype: int
         """
-        if self.rows == self.cols:  # check if matrix is square
-            if self.rows == 1:  # if matrix is 1x1
-                return self.matrix[0][0]
-            elif self.rows == 2:  # if matrix is 2x2
-                return self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0]
-            elif self.rows == 3:  # if matrix is 3x3
-                #  {det}(A)=a*e*i- a*f*h - b*d*i + b*f*g + c*d*h - c*e*g (check README for details)
-                diagonal = self.matrix[0][0] * self.matrix[1][1] * self.matrix[2][2]
-                afh = self.matrix[0][0] * self.matrix[1][2] * self.matrix[2][1]
-                bdi = self.matrix[0][1] * self.matrix[1][0] * self.matrix[2][2]
-                bfg = self.matrix[0][1] * self.matrix[1][2] * self.matrix[2][0]
-                cdh = self.matrix[0][2] * self.matrix[1][0] * self.matrix[2][1]
-                ceg = self.matrix[0][2] * self.matrix[1][1] * self.matrix[2][0]
-                res = diagonal - afh - bdi + bfg + cdh - ceg
-                return res
-            elif self.rows == 4:
-                pass
-
-    # Linear Transformation; Rotation
-    def rotate_2d_x(self, angle: float) -> Vector:
-        """
-        Rotate 2d matrix around x axis
-        :param angle: angle in degree
-        :type angle: float
-        :return: rotated matrix
-        :rtype: Matrix
-        """
-        # requires a vector
-        angle = angle * self.ANGLE_IN_RADIAN  # convert to radian
-        D2_MATRIX_ROTATION = [[math.cos(angle), -math.sin(angle)],
-                              [math.sin(angle), math.cos(angle)]]
-        if self.rows >= 1 and self.cols == 1:  # if vector size
-            return Matrix(self.matrix).__mul__(D2_MATRIX_ROTATION)
-        else:
-            raise TypeError("Matrix must be a vector")
-
-    def lu_decomposition(self):
-        pass
+        return self.rows, self.cols
 
     def rref(self, other):
         """
@@ -113,6 +77,113 @@ class Matrix(object):
         """
         pass
 
+    def rank(self):
+        """
+        Rank of the matrix
+        :return: rank
+        :rtype: int
+        """
+        pass
+
+    def inverse(self):
+        """
+        Inverse of the matrix
+        :return: inverse matrix
+        :rtype: Matrix
+        """
+        pass
+
+    def transpose(self):
+        """
+        Transpose of the matrix
+        :return: Transposed matrix
+        :rtype: Matrix
+        """
+        t: 'transpose matrix' = [list(a) for a in zip(*self.matrix)]
+        return __class__(*t)
+
+    def trace(self):
+        """
+        Trace of the matrix
+        :return: trace
+        :rtype: float
+        """
+        if self.rows != self.cols:
+            raise ValueError("Matrix must be square")
+        else:
+
+            trace = sum(self.matrix[i][i] for i in range(self.rows))
+        return trace
+
+    def determinant(self) -> float:
+        """
+        Determinant of the matrix
+        :return: determinant
+        :rtype: float
+        """
+        # todo: generalise the function for n using the determinant formula (check README)
+        if self.rows == self.cols:  # check if matrix is square
+            if self.rows == 1:  # if matrix is 1x1
+                return self.matrix[0][0]
+            elif self.rows == 2:  # if matrix is 2x2
+                return self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0]
+            elif self.rows == 3:  # if matrix is 3x3
+                #  det(A) = a*e*i- a*f*h - b*d*i + b*f*g + c*d*h - c*e*g (check README for details)
+                diagonal = self.matrix[0][0] * self.matrix[1][1] * self.matrix[2][2]
+                afh = self.matrix[0][0] * self.matrix[1][2] * self.matrix[2][1]
+                bdi = self.matrix[0][1] * self.matrix[1][0] * self.matrix[2][2]
+                bfg = self.matrix[0][1] * self.matrix[1][2] * self.matrix[2][0]
+                cdh = self.matrix[0][2] * self.matrix[1][0] * self.matrix[2][1]
+                ceg = self.matrix[0][2] * self.matrix[1][1] * self.matrix[2][0]
+                res = diagonal - afh - bdi + bfg + cdh - ceg
+                return res
+            elif self.rows == 4:
+                pass
+
+    # Linear Transformation; Rotation
+    def rotate_2d_x(self, angle: float):
+        """
+        Rotate 2d matrix around x axis
+        :param angle: angle in degree
+        :type angle: float
+        :return: rotated matrix
+        :rtype: Matrix
+        """
+        angle: float = angle * self.ANGLE_IN_RADIAN  # convert to radian
+        d2_matrix_rotation = ([math.cos(angle), -math.sin(angle)],
+                              [math.sin(angle), math.cos(angle)])
+        if self.rows >= 1 and self.cols == 1:  # if vector size equal
+            return __class__(self.matrix).__mul__(d2_matrix_rotation)
+        else:
+            raise TypeError("Matrix must be a vector")
+
+    def is_orthogonal(self) -> bool:
+        """
+        If A is a square matrix then : A is orthogonal iff A^TA = I
+        :return: True if orthogonal, False otherwise
+        :rtype: bool
+        """
+        if self.rows == self.cols:
+            return self.__mul__(self.transpose()) == Matrix.identity(self.rows)
+        else:
+            raise ValueError('{} is not a square matrix'.format(self.matrix))
+
+    def is_equivalent(self, other):  # if self (A) equivalent to other (B)
+        # Two matrices are equivalent iff they have the same reduced row- echelon form.
+        # todo: check if A and B are the same size
+        # todo: call rref or A and B then check if A and B are the same. if not return False
+        """
+        Check if two matrix are equivalent
+        :param other: other matrix
+        :type other: Matrix
+        :return: True if equivalent, False otherwise
+        :rtype: Boolean
+        """
+        pass
+
+    def lu_decomposition(self):
+        pass
+
     # Overloading operators
     def __add__(self, other):
         """
@@ -122,16 +193,34 @@ class Matrix(object):
         :return: Matrix
         :rtype: Matrix
         """
-        if self.rows == other.rows and self.cols == other.cols:  # check if dimensions are equal
-            # return Matrix(tuple([i + j for i, j in zip(self.matrix[i], other.matrix[i])] for i in range(self.rows))))
-            res = []
-            for i in range(self.rows):
-                res.append([])
-                for j in range(self.cols):
-                    res[i].append(self.matrix[i][j] + other.matrix[i][j])
+        # todo: test fails even though expected = actual
+        dim_self = self.dimension()
+        dim_other = other.dimension()
+        res = []
+        if dim_self == dim_other:
+            for (i, j) in zip(self, other):
+                res.append(list(map(lambda x, y: x + y, i, j)))
             return __class__(*res)
         else:
-            raise ValueError("Matrices must have the same dimensions")
+            raise ValueError("Matrix must be the same size")
+
+    def __sub__(self, other):
+        """
+        Subtraction operator
+        :param other: Matrix
+        :type other: Matrix
+        :return: Matrix
+        :rtype: Matrix
+        """
+        dim_self = self.dimension()
+        dim_other = other.dimension()
+        res = []
+        if dim_self == dim_other:
+            for (i, j) in zip(self, other):
+                res.append(list(map(lambda x, y: x - y, i, j)))
+            return __class__(*res)
+        else:
+            raise ValueError("Matrix must be the same size")
 
     def __str__(self):
         return str(self.matrix)
@@ -142,7 +231,47 @@ class Matrix(object):
     def __getitem__(self, key):
         return self.matrix[key]
 
+    def __eq__(self, other):
+        if self.rows == other.rows and self.cols == other.cols:
+            return self.matrix == other.matrix
+        else:
+            raise ValueError("Matrix must be the same size")
+
     def __mul__(self, other):
-        pass
+        """
+        Multiplication operator
+        :param other: Matrix or scalar
+        :type other: Matrix or (int,float)
+        :return: Matrix
+        :rtype: Matrix
+        """
+        if isinstance(other, (int, float)):
+            return __class__(*((list(other * x for x in row) for row in self.matrix)))
+        elif isinstance(other, Matrix) and self.cols == other.rows:
+            return self.__class__(*list(list(
+                Matrix.dot(row, col) for col in zip(*other.matrix))for row in self.matrix))
+        else:
+            raise ValueError("Matrix must be the same size")
 
+    # Class methods
+    @classmethod  # object reference not needed to call this method. only class
+    def identity(cls, rows):
+        """
+        Generates nxn identity matrix
+        :return: nxn identity matrix
+        :rtype: Matrix
+        """
+        if isinstance(rows, int) and rows == 0:
+            return __class__(cls([0]))
+        elif isinstance(rows, int) and rows > 0:
+            m = [[1 if i == j else 0 for j in range(rows)] for i in range(rows)]
+            return __class__(*m)
+        else:
+            raise ValueError("Matrix must be a square matrix")
 
+    @classmethod
+    def dot(cls, u, v):
+        if len(u) == len(v):
+            return sum([i * j for i, j in zip(u, v)])
+        else:
+            raise ValueError("Vector must be the same size")
--- Matrices /Matrix Class/test_matrix.py
+++ Matrices /Matrix Class/test_matrix.py
@@ -3,15 +3,72 @@
 # test_matrix.p_y
 # -----------------------------------------------------------------------
 import unittest
+import math
 from matrix import Matrix
 
 
 class MyTestCase(unittest.TestCase):
     """ Test cases for matrix.py """
 
+    def test_matrix_init(self):
+        pass
+
+    def test_equality(self):
+        """ Test equality function """
+        self.assertEqual(Matrix([1, 2], [3, 4]), Matrix([1, 2], [3, 4]))
+        self.assertFalse(Matrix([1, 2], [3, 4]) == Matrix([1, 2], [3, 5]))
+        self.assertRaises(ValueError, Matrix([1, 2], [3, 4]).__eq__, Matrix([1, 2], [3, 4], [5, 6]))
+
+    def test_dimension(self):
+        """ Test dimension function """
+        m = Matrix([1, 2, 3], [4, 5, 6])
+        self.assertEqual(m.dimension(), (2, 3))
+
+    def test_orthogonal(self):
+        """ Test orthogonal function """
+        self.assertTrue(Matrix([math.sqrt(3/2), -1/2], [1/2, math.sqrt(3/2)]).is_orthogonal())
+        self.assertFalse(Matrix([1, 1], [1, -1]).is_orthogonal())
+        self.assertTrue(Matrix([1, 0], [0, 1]).is_orthogonal())
+        self.assertRaises(ValueError, Matrix([1, 0], [0, 1], [1, 0]).is_orthogonal)
+        self.assertTrue(Matrix([1, 0, 0], [0, 1, 0], [0, 0, 1]).is_orthogonal())
+        self.assertFalse(Matrix([1, 2, 2], [2, 1, -2], [-2, 2, -1]).is_orthogonal())
+        self.assertTrue(Matrix([1/3, 2/3, 2/3], [2/3, 1/3, -2/3], [-2/3, 2/3, -1/3]).is_orthogonal())
+
+    def test_transpose(self):
+        self.assertEqual(Matrix([1, 2, 3], [4, 5, 6]).transpose(), Matrix([1, 4], [2, 5], [3, 6]))
+
+    def test_trace(self):
+        """ Test trace function """
+        A = Matrix([1, 2, 3], [4, 5, 6])
+        B = Matrix([1, 2, 3], [4, 5, 6], [7, 8, 9])
+        self.assertRaises(ValueError, A.trace)
+        self.assertEqual(B.trace(), 15)
+
     def test_add(self):
         """ Test add function """
-        self.assertEqual(Matrix([[1, 2], [3, 4]]).__add__(Matrix([[1, 2], [3, 4]])), Matrix([[2, 4], [6, 8]]))
+        self.assertEqual(Matrix([1, 2], [3, 4]).__add__(Matrix([1, 2], [3, 4])), Matrix([2, 4], [6, 8]))
+
+    def test_subtract(self):
+        """ Test subtract function """
+        self.assertEqual(Matrix([[1, 2], [3, 4]]).__sub__(Matrix([[1, 2], [3, 4]])), Matrix([[0, 0], [0, 0]]))
+
+    def test_multiply(self):
+        """ Test multiply function """
+        A = Matrix([1, 1, 0], [0, 0, 1])
+        B = Matrix([1, 0, 0], [2, 1, 0])
+
+        C = Matrix([1, 0], [0, 1])
+
+        D = Matrix([1, 1, 1],
+                   [0, 0, 1])
+        E = Matrix([1, 0, 1],
+                   [1, 1, 1],
+                   [0, 2, 0])
+
+        self.assertEqual(C.__mul__(2), Matrix([2, 0], [0, 2]))
+        self.assertEqual(A.__mul__(0), Matrix([0, 0, 0], [0, 0, 0]))
+        self.assertEqual(C.__mul__(0), Matrix([0, 0], [0, 0]))
+        self.assertEqual(D.__mul__(E), Matrix([2, 3, 2], [0, 2, 0]))
 
     def test_determinant(self):
         """ Test determinant function """
@@ -19,3 +76,18 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(Matrix([1, 2], [3, 4]).determinant(), -2)
         self.assertEqual(Matrix([1, 2, 3], [4, 5, 6], [7, 8, 9]).determinant(), 0)
         self.assertEqual(Matrix([3, 0, 1], [1, 2, 5], [-1, 4, 2]).determinant(), -42)
+
+    # ---------------------------------------------------------------------------------------------------------------------
+    # Clas Method Tests
+    def test_identity(self):
+        """ Test identity function """
+        self.assertEqual(Matrix.identity(2), Matrix([1, 0], [0, 1]))
+        self.assertEqual(Matrix.identity(3), Matrix([1, 0, 0], [0, 1, 0], [0, 0, 1]))
+        self.assertRaises(ValueError, Matrix.identity, -1)
+        self.assertEqual(Matrix.identity(4), Matrix([1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]))
+
+    def test_dot(self):
+        """ Test dot function """
+        u = [1, 2]
+        v = [1, 1]
+        self.assertEqual(Matrix.dot(u, v), 3)
